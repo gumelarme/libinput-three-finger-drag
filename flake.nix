d61f1ac02f5bd3d5939c7c836517056c7969e361
gumelarme <gumelar.pn@gmail.com>
2024-09-04 15:27:52 +0800

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.systems.url = "github:nix-systems/x86_64-linux";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-utils.inputs.systems.follows = "systems";
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          libinput
+          xdotool
+          gcc
+          rustc
+          cargo
+        ];
+      };
+    });
+}
